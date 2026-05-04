6c578f342d0827d19748904b5d099b4033f0a26c
imbrunosantoos <brunoribeirosouzasantos@gmail.com>
2026-05-04 23:55:57 +0100

    feat:add exercise 1

---
+++ q1/ex1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*Define a program that reads (using the scanf function) a sequence of integers ending with the number
+0 and prints the largest element of the sequence on the screen.*/
+
+#include <stdio.h>
+
+int main() {
+    int numero, maior;
+    int primeiro = 1; 
+
+    printf(" sequencia de numeros inteiros que termine com 0:\n");
+
+    while (1) {
+        scanf("%d", &numero);
+
+        if (numero == 0) {
+            break;
+        }
+        if (primeiro) {
+            maior = numero;
+            primeiro = 0;
+        } else {
+            if (numero > maior) {
+                maior = numero;
+            }
+        }
+    }
+
+    if (primeiro) {
+        printf("A sequencia estava vazia.\n");
+    } else {
+        printf("O maior elemento da sequencia e: %d\n", maior);
+    }
+
+    return 0;
+}
